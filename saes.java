335aa582d89c39776fc759edd420afcdbc54b822
DonHo0 <56067516+DonHo0@users.noreply.github.com>
2022-01-09 01:16:03 +0100

    [KeyGenerator]Finished the KeyGenerator

--- saes.java
+++ saes.java
@@ -10,6 +10,7 @@ public class saes {
     boolean [][]key={{true,true,false,false,true,true,true,true},{true,false,true,false,false,false,false,true}};
     boolean[][]xor=new boolean[2][8];
     int [][]xor1=new int[2][8];
+    boolean []tst={true,false,false,true};
     for(int i=0;i<8;i++){
         xor[0][i]=test[0][i]^key[0][i];
         xor[1][i]=test[1][i]^key[1][i];
@@ -24,7 +25,6 @@ public class saes {
     //ShiftRows(Sbox(xor));
     //ShiftRows(p);
     //boolean []tst={false,true,true,false};
-    //g(tst);
     key1(key);
     }
 public static boolean [][] ShiftRows (boolean [][]p){
@@ -73,15 +73,15 @@ public static boolean []g(boolean k[]){
     for(int j=0;j<k.length;j++){
         if(k[j]){
             b[j]=1;}
-        //System.out.println(k[j]+" K ");
+        //System.out.print(k[j]+" K ");
         }
     double b1[]={b[3],b[2]};
     double b2[]={b[1],b[0]};
     for(int l=0;l<2;l++){
-        rreshti=(int)(rreshti+(b2[l]*Math.pow(2,l)));
+        rreshti=(int)(rreshti+(b1[l]*Math.pow(2,l)));
     }
     for(int p=0;p<2;p++){
-        shtylla=(int)(shtylla+(b1[p]*Math.pow(2,p)));
+        shtylla=(int)(shtylla+(b2[p]*Math.pow(2,p)));
     }
     int x=sbox[shtylla][rreshti];
         //System.out.println(shtylla+" "+rreshti+" "+ x);
@@ -95,8 +95,8 @@ public static boolean []g(boolean k[]){
         for(int f=0;f<b.length;f++){
           if(b3[f]==1){
             b4[f]=true;}        
-           //System.out.println(b4[f]+" F ");     }
-          }
+           //System.out.print(b4[f]+" F ");     }
+        }
         return b4;    
     }
     
@@ -106,14 +106,20 @@ public static boolean [][]key1(boolean k[][]){
     boolean[] k1={k[0][4],k[0][5],k[0][6],k[0][7]};
     boolean[] k2={k[1][0],k[1][1],k[1][2],k[1][3]};
     boolean[] k3={k[1][4],k[1][5],k[1][6],k[1][7]};
+    boolean[] k3prim={k[1][4],k[1][5],k[1][6],k[1][7]};
     boolean[]k4=new boolean[4];
     boolean[]k5=new boolean[4];
     boolean[]k6=new boolean[4];
     boolean[]k7=new boolean[4];
-    boolean fg[]=g(k3);
+    boolean fg[]=g(k3prim);
     boolean[] r1={false,false,false,true};
     for(int l=0;l<4;l++){
         fg[l]=fg[l]^r1[l];}
+    /*for(int l=0;l<4;l++){
+        System.out.print(fg[l]+" fg ");}
+    for(int l=0;l<4;l++){
+        System.out.print(k3[l]+" k3 ");}*/
+    
     for(int j=0;j<k0.length;j++){
         k4[j]=k0[j]^fg[j];
         //System.out.println(k4[j]);    
@@ -129,43 +135,49 @@ public static boolean [][]key1(boolean k[][]){
     }
     boolean finalK1[][]={{k4[0],k4[1],k4[2],k4[3],k5[0],k5[1],k5[2],k5[3]},{k6[0],k6[1],k6[2],k6[3],k7[0],k7[1],k7[2],k7[3]}};
     for(int i=0;i<8;i++){
-        System.out.println(finalK1[0][i]+" final0 "+finalK1[1][i]+" ");
-    }
+        System.out.println(" rreshti 0 "+ finalK1[0][i]+" rreshti 1 "+finalK1[1][i]+" ");}
     return finalK1;}
-    public static boolean [][]key2(boolean k[][]){
-        boolean[] k4={k[0][0],k[0][1],k[0][2],k[0][3]};
-        boolean[] k5={k[0][4],k[0][5],k[0][6],k[0][7]};
-        boolean[] k6={k[1][0],k[1][1],k[1][2],k[1][3]};
-        boolean[] k7={k[1][4],k[1][5],k[1][6],k[1][7]};
-        boolean[]k8=new boolean[4];
-        boolean[]k9=new boolean[4];
-        boolean[]k10=new boolean[4];
-        boolean[]k11=new boolean[4];
-        boolean[] r2={false,false,true,false};
-        boolean fg1[]=g(k7);
-        for(int l=0;l<4;l++){
-            fg1[l]=fg1[l]^r2[l];}
-        for(int j=0;j<4;j++){
-                k8[j]=k4[j]^fg1[j];}
-        for(int j=0;j<4;j++){
-                k9[ j]=k5[j]^k8[j];}
-        for(int j=0;j<4;j++){
-                k10[j]=k6[j]^k9[j];}
-        for(int j=0;j<4;j++){
-                k11[j]=k7[j]^k10[j];}
-        boolean finalK2[][]={{k8[0],k8[1],k8[2],k8[3],k9[0],k9[1],k9[2],k9[3]},{k10[0],k10[1],k10[2],k10[3],k11[0],k11[1],k11[2],k11[3]}};
-        return finalK2;}
+
+public static boolean [][]key2(boolean k[][]){
+    boolean[] k4={k[0][0],k[0][1],k[0][2],k[0][3]};
+    boolean[] k5={k[0][4],k[0][5],k[0][6],k[0][7]};
+    boolean[] k6={k[1][0],k[1][1],k[1][2],k[1][3]};
+    boolean[] k7={k[1][4],k[1][5],k[1][6],k[1][7]};
+    boolean[] k7prim={k[1][4],k[1][5],k[1][6],k[1][7]};
+    boolean[]k8=new boolean[4];
+    boolean[]k9=new boolean[4];
+    boolean[]k10=new boolean[4];
+    boolean[]k11=new boolean[4];
+    boolean[] r2={false,false,true,false};
+    boolean fg1[]=g(k7prim);
+    for(int l=0;l<4;l++){
+        fg1[l]=fg1[l]^r2[l];}
+    for(int j=0;j<4;j++){
+            k8[j]=k4[j]^fg1[j];}
+    for(int j=0;j<4;j++){
+            k9[ j]=k5[j]^k8[j];}
+    for(int j=0;j<4;j++){
+            k10[j]=k6[j]^k9[j];}
+    for(int j=0;j<4;j++){
+            k11[j]=k7[j]^k10[j];}
+    boolean finalK2[][]={{k8[0],k8[1],k8[2],k8[3],k9[0],k9[1],k9[2],k9[3]},{k10[0],k10[1],k10[2],k10[3],k11[0],k11[1],k11[2],k11[3]}};
+    for(int i=0;i<8;i++){
+      System.out.println(" rreshti 0 "+ finalK2[0][i]+" rreshti 1 "+finalK2[1][i]+" ");}
+    
+    return finalK2;}
+
 public static boolean [][]key3(boolean k[][]){
     boolean[] k8={k[0][0],k[0][1],k[0][2],k[0][3]};
     boolean[] k9={k[0][4],k[0][5],k[0][6],k[0][7]};
     boolean[] k10={k[1][0],k[1][1],k[1][2],k[1][3]};
     boolean[] k11={k[1][4],k[1][5],k[1][6],k[1][7]};
+    boolean[] k11prim={k[1][4],k[1][5],k[1][6],k[1][7]};
     boolean[]k12=new boolean[4];
     boolean[]k13=new boolean[4];
     boolean[]k14=new boolean[4];
     boolean[]k15=new boolean[4];
     boolean r3[]={false,true,false,false};    
-    boolean fg2[]=g(k11);
+    boolean fg2[]=g(k11prim);
     for(int l=0;l<4;l++){
         fg2[l]=fg2[l]^r3[l];}        
     for(int j=0;j<4;j++){
@@ -176,11 +188,13 @@ public static boolean [][]key3(boolean k[][]){
             k14[j]=k10[j]^k13[j];}
     for(int j=0;j<4;j++){
             k15[j]=k11[j]^k14[j];}
-    boolean finalK3[][]={{k12[0],k12[1],k12[2],k12[3],k13[0],k13[1],k13[2],k13[3]},{k14[0],k14[1],k14[2],k14[3],k15[0],k15[1],k15[2],k15[3]}}; 
+    boolean finalK3[][]={{k12[0],k12[1],k12[2],k12[3],k13[0],k13[1],k13[2],k13[3]},{k14[0],k14[1],k14[2],k14[3],k15[0],k15[1],k15[2],k15[3]}};
+    for(int i=0;i<8;i++){
+        System.out.println(" rreshti 0 "+ finalK3[0][i]+" rreshti 1 "+finalK3[1][i]+" ");
+    } 
     return finalK3;
 }
-        
-           
+               
 public static boolean [][]Sbox(boolean p[][]){
     int [][]sbox={{6,11,0,4},{7,14,2,15},{9,8,10,12},{3,1,5,13}};
     int shtylla=0;
